7b99863d7009d89861efcd8716cb6e9d75a575b2
ayyti <123218709+ssinl@users.noreply.github.com>
2023-03-01 01:12:12 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.yppmro.cn'
+      url.hostname = 'www.ypiuuuuukfo.cn'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
